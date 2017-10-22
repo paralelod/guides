30c40de06f2fc107d29b2e7055b74cd4fc387cb9
paralelod <hello.diogorusso@gmail.com>
2017-10-22 17:31:35 -0200

    isitope lazy load fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,13 @@
 const gulp = require('gulp');
-const clean = require('gulp-clean');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
 const shell = require('gulp-shell');
 const browserSync = require('browser-sync').create();
 
-gulp.task('clean-scripts', function () {
-    return gulp.src('./assets/script.js', {read: false})
-      .pipe(clean());
-  });
-
-gulp.task('scripts', function() {
-    return gulp.src('./_assets/js/*.js')
-      .pipe(concat('script.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./assets/'));
-  });
 
 gulp.task('jekyll', shell.task(['bundle exec jekyll build --config "_config.yml,_config.dev.yml"']));
 
 gulp.task('jekyll-rebuild', ['jekyll'], function () {
     browserSync.reload();
 });
-
 
 gulp.task('serve', function () {
     browserSync.init({
@@ -33,22 +18,19 @@ gulp.task('serve', function () {
     });
     // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-    gulp.watch('./_assets/js/*.js', ['scripts']);
     gulp.watch([
         '_config.yml',
         '_config.dev.yml',
-        './**/*.html',
-        '!./_site/','./**/*.md', 
-        './_assets/scss/**/*.*',
-        './assets/script.js'
+        './**/**/*.html',
+        './**/**/*.md',
+        './**/**/*.scss',
+        '!./_site/', 
     ],
     ['jekyll-rebuild']);
 });
 
 gulp.task('default', 
     [
-        'clean-scripts', 
-        'scripts',
         'jekyll', 
         'serve'
     ]
