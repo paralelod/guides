494cbf951b7a80d0071b38fdb64029ba5d8b3254
paralelod <hello.diogorusso@gmail.com>
2018-03-24 22:28:48 -0300

    sw

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@ const PRECACHE_LIST = [
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
   "https://hello.diogorusso.com",
-  "https://lab.diogorusso.com/",
+  "https://lab.diogorusso.com/"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
