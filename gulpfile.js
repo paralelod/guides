e8432536d24c9ec43e3b695bc227db5675d3cff7
paralelod <hello.diogorusso@gmail.com>
2018-03-27 23:32:15 -0300

    slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,6 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-const browserSync = require('browser-sync').create();
-
-var gulpCloudinary = require('gulp-cloudinary');
-var config = { 
-	cloud_name: 'diogorusso', 
-	api_key: '539963394324974', 
-	api_secret: 'LbSkIqejQ8ApC_lABEFCEvQz1-U'
-};
-
-var defaultTags = ['projectX1'];
-
-var builderDefault = new gulpCloudinary(config, defaultTags);
-
-gulp.task('clean', function(){
-	builderDefault.deleteOldByTag();
-});
-
-gulp.task('upload_images', function() {
-  	return gulp.src(['.assets/img/**/**/*.jpg'])
-  		.pipe(builderDefault.uploader())
-  		.pipe(gulp.dest('/bio'))
-});
-
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
 
 gulp.task('jekyll', shell.task(['bundle exec jekyll build --config "_config.yml,_config.dev.yml"']));
 
@@ -33,8 +11,13 @@ gulp.task('jekyll-rebuild', ['jekyll'], function () {
 gulp.task('serve', function () {
     browserSync.init({
         logConnections: true,
+        https: true,
         server: {
-            baseDir: '_site/'
+            baseDir: '_site/',
+            middleware: function (req, res, next) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                next();
+              }
         }
     });
     // Reloads page when some of the already built files changed:
